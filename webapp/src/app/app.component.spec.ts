acdffa7a18dce59753635e47a5a82acffdd79e92
alexander.peter <alexander.peter@stud.hslu.ch>
2020-02-05 20:18:35 +0100

    format all files in workspace

--- webapp/src/app/app.component.spec.ts
+++ webapp/src/app/app.component.spec.ts
@@ -1,9 +1,9 @@
-import {TestBed, async} from '@angular/core/testing';
-import {AppComponent} from './app.component';
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-    beforeEach(async (() => {
-        TestBed.configureTestingModule({declarations: [AppComponent]}).compileComponents();
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({ declarations: [AppComponent] }).compileComponents();
     }));
 
     it('should create the app', () => {
