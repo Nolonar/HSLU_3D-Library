b52f41cef3bca8b8e7fd15bd0a8eb72a28cfc96a
alexander.peter <alexander.peter@stud.hslu.ch>
2020-02-06 20:21:34 +0100

    added .env file

---
+++ webapp/set-env.ts
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const colors = require('colors');
+require('dotenv').load();
+
+const targetPath = './src/environments/environment.ts';
+
+const envConfigFile =
+    `export const environment = {
+    DB_USER: '${process.env.DB_USER}',
+    DB_PASSWORD: '${process.env.DB_PASSWORD}',
+    production: '${process.env.PRODUCTION}'
+};
+`;
+fs.writeFile(targetPath, envConfigFile, function (err) {
+    if (err) {
+        throw console.error(err);
+    } else {
+        console.log(colors.magenta(`Angular environment.ts file generated correctly at ${targetPath} \n`));
+    }
+});
