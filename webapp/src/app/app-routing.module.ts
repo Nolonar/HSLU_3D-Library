89063b888c1317dbc2e579aab4d011e5cbb44186
Wyrsch Léonard <leonard.wyrsch@stud.hslu.ch>
2020-02-07 09:56:22 +0100

    remove redirection

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { OverviewComponent } from './overview/overview.component'
-import { DetailComponent } from './detail/detail.component'
-import { NotFoundComponent } from './not-found/not-found.component'
+import { OverviewComponent } from './overview/overview.component';
+import { DetailComponent } from './detail/detail.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/overview', pathMatch: 'full' },
-  { path: 'overview', component: OverviewComponent },
+  { path: '', component: OverviewComponent },
   { path: 'detail/:id', component: DetailComponent },
   { path: '404', component: NotFoundComponent },
   { path: '**', redirectTo: '/404' }
