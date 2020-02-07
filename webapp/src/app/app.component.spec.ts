4c8cb74f54069ad6aba95b21a62f607b644b5cdd
alexander.peter <alexander.peter@stud.hslu.ch>
2020-02-07 11:10:09 +0100

    add routing to test

--- webapp/src/app/app.component.spec.ts
+++ webapp/src/app/app.component.spec.ts
@@ -1,9 +1,17 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
     beforeEach(async(() => {
-        TestBed.configureTestingModule({ declarations: [AppComponent] }).compileComponents();
+        TestBed.configureTestingModule({
+            imports: [
+                RouterTestingModule
+            ],
+            declarations: [
+                AppComponent
+            ],
+        }).compileComponents();
     }));
 
     it('should create the app', () => {
@@ -16,12 +24,5 @@ describe('AppComponent', () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.debugElement.componentInstance;
         expect(app.title).toEqual('webapp');
-    });
-
-    it('should render title', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        fixture.detectChanges();
-        const compiled = fixture.debugElement.nativeElement;
-        expect(compiled.querySelector('.content span').textContent).toContain('webapp app is running!');
     });
 });
