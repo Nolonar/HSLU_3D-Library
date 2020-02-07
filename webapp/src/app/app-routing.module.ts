370338801600ddf87c4e0e3056258efbf0d3945a
Wyrsch Léonard <leonard.wyrsch@stud.hslu.ch>
2020-02-07 09:21:36 +0100

    add app skeleton (overview, detail, 404, and rout)

---
+++ webapp/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { OverviewComponent } from './overview/overview.component'
+import { DetailComponent } from './detail/detail.component'
+import { NotFoundComponent } from './not-found/not-found.component'
+
+const routes: Routes = [
+  { path: '', redirectTo: '/overview', pathMatch: 'full' },
+  { path: 'overview', component: OverviewComponent },
+  { path: 'detail/:id', component: DetailComponent },
+  { path: '404', component: NotFoundComponent },
+  { path: '**', redirectTo: '/404' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DetailComponent } from './detail/detail.component';
+import { AppRoutingModule } from './app-routing.module';
+import { OverviewComponent } from './overview/overview.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        DetailComponent,
+        OverviewComponent,
+        NotFoundComponent
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        AppRoutingModule
     ],
     providers: [],
     bootstrap: [AppComponent]
