9f4ae86ca4c26059a6adad35b51d263c97e66a47
alexander.peter <alexander.peter@stud.hslu.ch>
2020-02-09 00:09:03 +0100

    add button and icons

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DetailComponent } from './detail/detail.component';
@@ -19,7 +20,8 @@ import { ViewerComponent } from './viewer/viewer.component';
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+        FontAwesomeModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
