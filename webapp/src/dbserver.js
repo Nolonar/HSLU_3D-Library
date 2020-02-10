389d5b9a7fc6e80a9a27dc3eae0e2b59de0b893d
alexander.peter <alexander.peter@stud.hslu.ch>
2020-02-10 09:52:06 +0100

    check if properties are available

--- webapp/src/dbserver.js
+++ webapp/src/dbserver.js
@@ -7,7 +7,7 @@ const frontendDomain = 'http://localhost:4200';
  * Since this is backend only, must be run BEFORE the frontend (ng serve).
  * Otherwise, frontend will receive no data from database.
  * 
- * Remeber to edit frontendDomain for CORS (Cross-Origin Ressource Sharing).
+ * Remember to edit frontendDomain for CORS (Cross-Origin Ressource Sharing).
  */
 
 require('dotenv').load();
@@ -17,11 +17,30 @@ const app = require('express')();
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 const dbDomain = process.env.DB_DOMAIN;
-// Opening a connection takes time, so we only do it once when the server starts, and never close it.
-let client = MongoClient.connect(`mongodb+srv://${dbUser}:${dbPassword}@${dbDomain}/test?retryWrites=true&w=majority`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+let propertiesAvailable = true;
+
+if (!dbUser) {
+    console.warn(`Environment property 'DB_USER' is missing: `);
+    propertiesAvailable = false;
+}
+if (!dbPassword) {
+    console.warn(`Environment property 'DB_PASSWORD' is missing: `);
+    propertiesAvailable = false;
+}
+if (!dbDomain) {
+    console.warn(`Environment property 'DB_USER' is missing: `);
+    propertiesAvailable = false;
+}
+if (propertiesAvailable) {
+    console.log(`starting db server...`);
+    // Opening a connection takes time, so we only do it once when the server starts, and never close it.
+    let client = MongoClient.connect(`mongodb+srv://${dbUser}:${dbPassword}@${dbDomain}/test?retryWrites=true&w=majority`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+} else {
+    throw 'Server can not be started due to missing properties';
+}
 
 async function getDb() {
     return (await client).db('test');
@@ -38,8 +57,6 @@ app.use((req, res, next) => {
 });
 
 app.get('/models', async (req, res) => {
-    console.log('GET /models');
-
     const collection = await getModelsCollection();
     const allModels = await collection.find({}).toArray();
     res.send(JSON.stringify(allModels));
@@ -47,8 +64,6 @@ app.get('/models', async (req, res) => {
 
 app.get('/model/:modelId', async (req, res) => {
     const modelId = req.params['modelId'];
-    console.log('GET /model/' + modelId);
-
     const collection = await getModelsCollection();
     const model = await collection.findOne({ '_id': +modelId });
     res.send(JSON.stringify(model));
