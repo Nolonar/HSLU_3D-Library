90f1691207be32abc13dc28642981ac6fcd67b65
alexander.peter <alexander.peter@stud.hslu.ch>
2020-02-13 00:18:19 +0100

    adjust login to upload form

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -6,11 +6,11 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DetailComponent } from './detail/detail.component';
+import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { OverviewComponent } from './overview/overview.component';
 import { UploadFormComponent } from './upload-form/upload-form.component';
 import { ViewerComponent } from './viewer/viewer.component';
-import { LoginComponent } from './login/login.component';
 
 @NgModule({
     declarations: [
--- webapp/src/app/login/login.component.ts
+++ webapp/src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-login',
@@ -7,31 +7,35 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  loginForm: FormGroup;
+  faWarning = faExclamationTriangle;
+  public name = '';
+  public password = '';
 
   constructor(
-    private formBuilder: FormBuilder,
+    // private formBuilder: FormBuilder,
     // private authService: AuthService,
   ) { }
 
   ngOnInit(): void {
-    this.loginForm = this.formBuilder.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required]
-    });
+    // empty
+  }
+
+  public onSubmit(event) {
+    console.log('onsubmit');
   }
 
   login() {
-    const val = this.loginForm.value;
+    console.log('login');
+    // const val = this.loginForm.value;
 
-    if (val.username && val.password) {
-      // this.authService.login(val.username, val.password)
-      //   .subscribe(
-      //     () => {
-      console.log(`User${val.username} is logged in`);
-      //       this.router.navigateByUrl('/');
-      //     }
-      //   );
-    }
+    // if (val.username && val.password) {
+    // this.authService.login(val.username, val.password)
+    //   .subscribe(
+    //     () => {
+    // console.log(`User${val.username} is logged in`);
+    //       this.router.navigateByUrl('/');
+    //     }
+    //   );
+    // }
   }
 }
